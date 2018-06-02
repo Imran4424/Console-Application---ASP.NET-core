94f62dda348600bd1082e9c362244de6fd594541
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-06-02 21:46:35 +0600

    console application ASP.NET core

---
+++ ConsoleApplication/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication
+{
+    class Student
+    {
+        public int studentId { get; set; }
+    }
+}
